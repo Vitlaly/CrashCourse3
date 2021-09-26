15bb55ae1260a9b2fe8ef1c91430c67190d568fa
Vitaly <onychkov@gmail.com>
2021-09-26 23:11:14 +0300

    commit

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_orders = orders.count()
+    total_customers = customers.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    product = Product.objects.all()
+
+    return render(request, 'accounts/products.html', {'product': product})
 
 
 def customer(request):
